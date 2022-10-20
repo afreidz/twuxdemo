8e27d3e28a95c73ef83745207ed9bec879c52f7a
Andy Freidenfelds <afreidenfelds@hitachisolutions.com>
2022-10-20 12:05:06 -0500

    final product

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,45 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        branding: {
+          blue: {
+            200: "#E9F1FF",
+            400: "#4285F4",
+            500: "#0089ED",
+          },
+          neutral: {
+            200: "#F2F2F2",
+            500: "#8D8D8D",
+          }
+        }
+      },
+      fontFamily: {
+        sans: ["Poppins", 'ui-sans-serif']
+      },
+      fontSize: {
+        sm: ['13px', '20px'],
+        base: ['14px', '21px'],
+        huge: ['40px', {
+          lineHeight: '60px',
+          fontWeight: 500,
+        }],
+      },
+      spacing: {
+        15: "3.8125rem",
+      },
+      borderRadius: {
+        lg: "40px",
+        md: "9px",
+      },
+      minHeight: {
+        panel: "740px",
+      },
+      maxWidth: {
+        panel: "326px",
+      }
+    },
   },
   plugins: [],
 }
